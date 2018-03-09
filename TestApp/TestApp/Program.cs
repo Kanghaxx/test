3659eb2d598e934f4253a9968691435e35344291
karasi-home\karasi <epicdevelop@gmail.com>
2018-03-10 01:16:56 +0300

    Add test code

--- TestApp/TestApp/Program.cs
+++ TestApp/TestApp/Program.cs
@@ -12,6 +12,7 @@ namespace TestApp
         {
             Console.WriteLine("Started");
             Console.WriteLine("Test code");
+            Console.WriteLine("Test code 2");
             Console.WriteLine("Stopped");
             Console.ReadKey();
         }
